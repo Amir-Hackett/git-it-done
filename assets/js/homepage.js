f8ea0eaa73ab847dfa2846fa3166072976c7d842
Amir <amir.hackett@gmail.com>
2022-03-23 19:52:28 -0400

    added form to html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var getUserRepos = function(user){
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos"
 
-    // make a request to the url
+    // makes a request to the url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
             console.log(data)
