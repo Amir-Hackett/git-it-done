9b22a620f2091ec9dd96af792da1187a3e2f81d0
Amir <amir.hackett@gmail.com>
2022-03-24 17:39:30 -0400

    displayed repos on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form")
 var nameInputEl = document.querySelector("#username")
+var repoContainerEl = document.querySelector("#repos-container")
+var repoSearchTerm = document.querySelector("#repo-search-term")
 
 var getUserRepos = function(user){
     // format the github api url
@@ -8,7 +10,7 @@ var getUserRepos = function(user){
     // makes a request to the url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
-            console.log(data)
+            displayRepos(data, user)
         })
     })
 }
@@ -23,6 +25,28 @@ var formSubmitHandler = function(event) {
         nameInputEl.value = ""
     } else {
         alert("Please enter a GitHub username")
+    }
+}
+
+var displayRepos = function(repos, searchTerm){
+    // loop over repos
+    for (var i = 0; i < repos.length; i++){
+        // formats repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name
+
+        // create a container for each repo
+        var repoEl = document.createElement("div")
+        repoEl.classList = "list-item flex-row justify-space-between align-center"
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span")
+        titleEl.textContent = repoName
+
+        // append to container
+        repoEl.appendChild(titleEl)
+
+        // append container to DOM
+        repoContainerEl.appendChild(repoEl)
     }
 }
 
