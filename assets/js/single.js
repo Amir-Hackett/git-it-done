dcad63203a117d1fc0efa31a7cd07752d3e2b707
Amir <amir.hackett@gmail.com>
2022-03-25 15:59:22 -0400

    added DOM elements for issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,16 +1,56 @@
+var issueContainerEl = document.querySelector("#issues-container")
+
 var getRepoIssues = function(repo) {
     var apiURL = "https://api.github.com/repos/" + repo + "/issues?directions=asc"
     fetch(apiURL).then(function(response){
         // request was successful
         if(response.ok){
             response.json().then(function(data){
-                console.log(data)
+                // passes response data to dom function
+                displayIssues(data)
             })
         }else {
             alert("There was a problem with your request!")
         }
     })
-    console.log(repo);
 }
 
-getRepoIssues("facebook/react")
+var displayIssues = function(issues){
+// if no open issues
+if (issues.length === 0){
+    issueContainerEl.textContent = "This repo has no open issues!"
+    return
+}
+
+// if there are open issues
+for(var i = 0; i < issues.length; i++){
+    // create a link element to take uses to get hib
+    var issueEl = document.createElement("a")
+    issueEl.classList = "list-item flex-row justify-space-between align-center"
+    issueEl.setAttribute("href", issues[i].html_url)
+    issueEl.setAttribute("target", "_blank")
+
+    // create span to hold issue title
+    var titleEl = document.createElement("span")
+    titleEl.textContent = issues[i].title
+
+    // append to container
+    issueEl.appendChild(titleEl)
+
+    // create a type element
+    var typeEl = document.createElement("span")
+
+    // check if issues is an actual issues or a pull request
+    if (issues[i].pull_request){
+        typeEl.textContent = "(Pull Request)"
+    } else {
+        typeEl.textContent = "(Issue)"
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl)
+    issueContainerEl.appendChild(issueEl)
+}
+}
+
+getRepoIssues("react")
